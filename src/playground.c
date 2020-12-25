eb918ca4167cf61ada95cb5b8718f761bbe786c2
senlyu <sen.lyu@uconn.edu>
2020-12-25 10:35:19 -0500

    test

---
+++ src/playground.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Variable declaration:
+extern int a, b;
+extern int c;
+extern float f;
+
+int main() {
+    /* variable definition: */
+    int a,b,c;
+    float f;
+
+    /* actual init */
+    a = 10;
+    b = 20;
+    c = a + b;
+    printf("value of c : %d \n", c);
+
+    f = 70.0 / 3.0;
+    printf("value of f : %f \n", f);
+
+    return 0;
+}
